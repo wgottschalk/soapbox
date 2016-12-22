bb5935a0560f7381f6e76cad703ad666603c0bb3
wgottschalk-RMS <Will.Gottschalk@rms.com>
2016-12-21 21:11:32 -0800

    pushing to heroku

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,8 @@ config :soapbox, Soapbox.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :soapbox, Soapbox.Repo
+
+config :soapbox, Soapbox.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL")
 
